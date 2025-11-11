21423f93f7ae5c2cdebf8016528226f1b19a04a1
OficerKot <razyice@mail.ru>
2025-11-11 13:52:44 +1000

    feat: добавлен новый тип домино (с освещением)
    - Fireflies
    - Изменено хранение информации о домино, теперь используется scriptable object DominoData и DominoManager
    - Упрощён процесс добавления новых видов домино

---
+++ Assets/Scripts/Domino/DominoManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DominoManager", menuName = "Domino/DominoManager")]
+
+public class DominoManager : ScriptableObject
+{
+    public DominoData[] allDomino;
+    public static DominoManager Instance;
+
+    private void OnEnable()
+    {
+        Instance = this;
+    }
+    public DominoData GetDominoByID(string id)
+    {
+        return System.Array.Find(allDomino, domino => domino.dominoId == id);
+    }
+
+    public DominoData GetRandomDomino()
+    {
+        int indx = Random.Range(0, allDomino.Length);
+        return allDomino[indx];
+    }
+
+}
+public enum Image
+{
+    any, bone, fireflies
+}
