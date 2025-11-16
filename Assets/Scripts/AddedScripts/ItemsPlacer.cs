e4d31d0415f9d27aefd8335877757ef0c0f43b5e
Daniel_Firefox <horrorassociation10@gmail.com>
2025-11-16 18:10:55 +1000

    ItemsRandomSpawn
    Старый скрипт точечного раскидывания тайлов по карте был переделан под точечное раскидывание предметов по карте. Подтянут поднастроенный PerlinNoiseMap скрипт из предыдущего коммита

---
+++ Assets/Scripts/AddedScripts/ItemsPlacer.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemsPlacer : MonoBehaviour
+{
+    int map_width;
+    int map_height;
+
+    public int items_count = 10;
+    
+    public GameObject[] itemTypes;
+    private int itemsCount;
+    private PerlinNoiseMap perlin;
+    private List<List<int>> noise_grid;
+
+    void Awake()
+    {
+        perlin = this.GetComponent<PerlinNoiseMap>();
+        noise_grid = perlin.GetNoiseGrid();
+    }
+
+    public void PlaceItems(int width, int height)
+    {
+        Debug.Log("PlaceItems called");
+        map_width = width;
+        map_height = height;
+        itemsCount = itemTypes.Length;
+        Debug.Log($"Length = {itemTypes.Length}");
+        GenerateTiles(items_count);
+        Debug.Log("Generated!");
+    }
+
+    void GenerateTiles(int items_count)
+    {
+        for (int i  = 0; i<items_count; ++i)
+        {
+            int x = Random.Range(0, map_width);
+            int y = Random.Range(0, map_height);
+            
+            if (noise_grid[x][y] == 0 || noise_grid[x][y] == 1)
+            {
+                CreateItem(x, y);
+            }
+            else
+            {
+                --i;
+                continue;
+            }
+            
+        }
+    }
+    void CreateItem(int x, int y)
+    {
+        int item_id = Random.Range(0, itemsCount-1);
+        GameObject item_prefab = itemTypes[item_id];
+        GameObject item = Instantiate(item_prefab, this.transform);
+
+        item.name = string.Format("item_{0}_x{1}_y{2}", item_prefab.name, x, y);
+        item.transform.localPosition = new Vector3Int(x, y, 0);
+    }
+}
